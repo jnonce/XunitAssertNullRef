9f4feb756a11129fc10b80d777fe5aba90bbc616
jnonce <jnonce@outlook.com>
2017-08-29 08:29:55 -0700

    Demo an issue with AllException.Message which impacts runners

---
+++ AssertAll.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+using Xunit.Sdk;
+
+namespace XunitAssertNullRef
+{
+    public class AssertAll
+    {
+        [Fact]
+        public void WillNeverProduceResult()
+        {
+            object[] collection = new object[]
+            {
+                new object(),
+                null
+            };
+
+            Assert.All(collection, Assert.NotNull);
+        }
+
+        [Fact]
+        public void ThrownExceptionProducesNullRef()
+        {
+            AllException exception = Assert.Throws<AllException>(() => WillNeverProduceResult());
+
+            Assert.Throws<NullReferenceException>(() => exception.Message);
+        }
+    }
+}
